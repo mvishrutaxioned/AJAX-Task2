0ab153ce37e88509d557cfa4a8db4ac839c00d80
VishrutMargam <vishrutm@axioned.com>
2021-12-01 17:17:49 +0530

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
 
+    // variables
+    var inputVal;
+    var today = new Date();
+    var d = String(today.getDate()).padStart(2, '0');
+    var m = String(today.getMonth() + 1).padStart(2, '0');
+
+    // months array
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
     // on form submit function
     $('form').submit((e) => {
