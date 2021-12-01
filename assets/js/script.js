f2854fc82821b83bd5b5ff0934c39c7599cecefd
VishrutMargam <vishrutm@axioned.com>
2021-12-01 17:18:07 +0530

    added fetch results function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,13 @@ $(document).ready(() => {
     // months array
     var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
+    // fetch results function
+    async function fetchResults(i) {
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${i}&appid=4b9ffb45e1734ffac3f5a513794a0f0d`
+        var data = await fetch(url).then(res => res.json()).catch(e => e)
+        displayWeather(data)
+    }
+
     // on form submit function
     $('form').submit((e) => {
         e.preventDefault();
